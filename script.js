def2d0284f2fd9fcb20fbca0d30dec477022a37a
Cgotts <cobi.gottschalk@gmail.com>
2022-12-30 01:00:04 -0800

    Added stuff to full stack

--- script.js
+++ script.js
@@ -51,8 +51,61 @@ function myFunction() {
 
 
 
+// $(".carousel").carousel({
+//   interval: 3000
+// });
+
+// //scroll slides on swipe for touch enabled devices
+// $(".carousel").on("touchstart", function(event) {
+//   var yClick = event.originalEvent.touches[0].pageY;
+//   $(this).one("touchmove", function(event) {
+//     var yMove = event.originalEvent.touches[0].pageY;
+//     if (Math.floor(yClick - yMove) > 1) {
+//       $(".carousel").carousel("next");
+//     } else if (Math.floor(yClick - yMove) < -1) {
+//       $(".carousel").carousel("prev");
+//     }
+//   });
+//   $(".carousel").on("touchend", function() {
+//     $(this).off("touchmove");
+//   });
+// });
 
 
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
 
 
 
